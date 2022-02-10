e6d6553460dd0ad248c5e16adc65c01f5a7f112d
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-10 17:50:20 -0500

    creando logica del cambiado de color

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const main = document.getElementById('main')
 const HEX_NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const HEX_ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F']
 const RGB = {
@@ -53,9 +54,29 @@ const getColorRGB = () => {
 }
 
 btnChange.addEventListener('click', (e) => {
-    
-    console.log(generateRamdonHEX())
-    console.log(getColorNormal())
-    console.log(getColorRGB())
+
+    const number = getRandomInt(0, 2)
+    const ramdonEjecution = [getColorNormal, getColorRGB]
+
+    localStorage.setItem('hex', generateRamdonHEX() )
+    localStorage.setItem('simple', JSON.stringify( ramdonEjecution[number]() ) )
+
+    /**
+     * if (localStorage.getItem('hex') !== null) {
+        main.style.backgroundColor = localStorage.getItem('hex')
+        }
+     */
+
+    if (localStorage.getItem('simple') !== null) {
+        const getData= JSON.parse( localStorage.getItem('simple') )
+        const result =  typeof getData === 'object'
+            ? `rgb(${getData.red}, ${getData.green}, ${getData.blue})`
+            : getData 
+        console.log(result)
+        main.style.backgroundColor = result
+
+    }
+
+
 
 })
